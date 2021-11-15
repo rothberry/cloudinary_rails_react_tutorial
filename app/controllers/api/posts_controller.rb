53661bddd416051ea9f38bb2704a2347ad5696ae
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-15 11:48:56 -0800

    add routes/controllers for users/posts/sessions

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,5 @@
+class Api::PostsController < ApplicationController
+  def index
+    render json: Post.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    resources :posts, only: [:index]
+
+    get "/me", to: "users#show"
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
