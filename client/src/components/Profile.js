2060cba1a6c1093ddda88a46fd4e37add2865110
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-16 16:43:00 -0800

    add CloudinaryUpload component to Profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import CloudinaryUpload from './CloudinaryUpload'
+import CloudinaryUpload from './CloudinaryUpload'
 
 function Profile({currentUser, setCurrentUser}) {
   const handleUpload = (result) => {
     const body = {
       profile_picture_url: result.info.secure_url,
       profile_picture_thumbnail_url: result.info.eager[0].secure_url,
-      cloudinary_public_id: 'fill me in'
+      cloudinary_public_id: result.info.public_id
     }
     fetch('/api/me', {
       method: 'PATCH',
@@ -26,11 +26,11 @@ function Profile({currentUser, setCurrentUser}) {
     <div className="grid sm:grid-cols-3 gap-4">
       <div className="">
         <img src={currentUser.profile_picture_url || "https://res.cloudinary.com/dpkrqs9rs/image/upload/v1637085098/Profile_avatar_placeholder_large_ky4gfw.png"} />
-        {/* <CloudinaryUpload
-          preset="tl8ilpaq"
+        <CloudinaryUpload
+          preset="zbvx4nwo"
           buttonText="Update Profile Picture"
           handleUpload={handleUpload}
-        /> */}
+        />
       </div>
       <div className="col-span-2">
         <h2 className="text-xl">{currentUser.username}</h2>
